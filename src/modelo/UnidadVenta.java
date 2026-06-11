6ecc9664ad2c4bf7b6fabb679446647b5878931d
Sebastian Damonte <sdamonte365@gmail.com>
2026-06-10 21:20:21 -0300

    .

--- src/modelo/UnidadVenta.java
+++ src/modelo/UnidadVenta.java
@@ -86,7 +86,32 @@ public abstract class UnidadVenta {
 	public void setEstado(boolean estado) {
 		this.estado = estado;
 	}
+//int idPlato, String nombre, float precioVenta, float costroProd
+	
+	public Plato traerPlato(String nombre) {
+		Plato plato = null;
+		int i=0;
+		while(i<this.getLstPlatos().size() && plato == null) {
+			if(this.getLstPlatos().get(i).getNombre().equalsIgnoreCase(nombre)) {
+				plato = getLstPlatos().get(i);
+			}
+			i++;
+		}
+		return plato;
+	}
+	
+	public boolean agregarPlato (String nombre, float precioVenta, float costoProd)throws Exception{
+		
+		if(traerPlato(nombre)!=null) {
+			throw new Exception("Este plato ya existe");
+		}
+		
+		int id = this.lstPlatos.isEmpty() ? 1 : this.lstPlatos.get(this.lstPlatos.size() - 1).getIdPlato() + 1;
 
+		Plato p = new Plato(id, nombre, precioVenta, costoProd);
+		
+		return lstPlatos.add(p);
+	}
 
 	@Override
 	public String toString() {
@@ -97,26 +122,26 @@ public abstract class UnidadVenta {
 	           ", codigo=" + codigo + 
 	           ", lstPlatos=" + lstPlatos + 
 	           ", lstPersonal=" + lstPersonal + 
-	           ", estado=" + estado + "]";
+	           ", estado=" + estado + "]\n";
 	}
 	public boolean equals(UnidadVenta unidadVenta) {
 		return this.getCodigo().equalsIgnoreCase(unidadVenta.getCodigo());
 	}
 	
 	//CASO DE USO 2 - Localizar cualquier entidad del sistema.
-	public Plato traerPlato(int idPlato) {
-		Plato p = null;
-		int i = 0;
-		
-		while(i < this.lstPlatos.size() && p == null) {
-			if(this.lstPlatos.get(i).getIdPlato() == idPlato) {
-				p = this.lstPlatos.get(i);
-			}
-			i++;
-		}
-		
-		return p;
-	}
+//	public Plato traerPlato(int idPlato) {
+//		Plato p = null;
+//		int i = 0;
+//		
+//		while(i < this.lstPlatos.size() && p == null) {
+//			if(this.lstPlatos.get(i).getIdPlato() == idPlato) {
+//				p = this.lstPlatos.get(i);
+//			}
+//			i++;
+//		}
+//		
+//		return p;
+//	}
 	
 	public abstract float calcularCanon();
 	
--- src/test/Test.java
+++ src/test/Test.java
@@ -33,116 +33,157 @@ public class Test {
 		try {
 			s.agregarCajero("Sebastian", "Damonte", 47130790, LocalDate.of(2006, 4, 22), LocalDate.of(2026, 5, 21), "Cajero", "Mañana", true);
 			s.agregarCajero("Thiago", "Dagraca", 47130890, LocalDate.of(2000, 5, 21), LocalDate.of(2021, 4, 21), "Cajero", "Noche", false);
+			
+			s.agregarCajero("Santiago", "Brest", 45263123, LocalDate.of(2002, 4, 14), LocalDate.of(2023, 12, 5), "Cajero", "Mañana", true);//
+			s.agregarCajero("Federico", "Lopez", 36211589, LocalDate.of(1995, 8, 7), LocalDate.of(2020, 12, 4), "Cajero", "Noche", false);//
 		}catch(Exception e) {System.out.println(e.getMessage());}
 		
-		Cajero c1 = (Cajero) s.traerPersonal(47130790);
-		Cajero c2= (Cajero) s.traerPersonal(47130890);
+		
 		
 		try {
 			s.agregarCocinero("Santiago", "Filipeli", 46123456, LocalDate.of(2003, 2, 26), LocalDate.of(2024, 1, 16), "Cocinero", "Parrilla", 1400.0f, true);
 			s.agregarCocinero("Martin", "Torrens", 45258369, LocalDate.of(2001, 3, 25), LocalDate.of(2022, 6, 6), "Cocinero", "Italiana", 2600.5f, true);
+			
+			s.agregarCocinero("Nahuel", "Garcia", 35412698, LocalDate.of(1998, 8, 9), LocalDate.of(2019, 5, 7), "Cocinero", "Heladeria", 900.5f, true);//
+			s.agregarCocinero("Mateo", "Proruk", 31258741, LocalDate.of(2005, 3, 31), LocalDate.of(2026, 2, 14), "Cocinero", "Milkshakes", 918.5f, true);//
 		}catch(Exception e) {System.out.println(e.getMessage());}
 		
-		Cocinero co1 = (Cocinero) s.traerPersonal(46123456);
-		Cocinero co2 = (Cocinero) s.traerPersonal(45258369);
+		
 		
 		System.out.println(s.getLstPersonal());
 		
 		//	-----------------------------
 		
 		try {
-			s.agregarPuestoDesarmable("El Parrillon", co2, 20, "abcde12345", true, 2,75);
-			s.agregarFoodTruck("Luiggi", co2, 30, "qwert45678", true, "dsz429" , true);
+			s.agregarPuestoDesarmable("El Parrillon", s.traerPersonal(45258369), 20, "abcde12345", true, 2,75);
+			s.agregarFoodTruck("Luiggi", s.traerPersonal(46123456), 30, "qwert45678", true, "dsz429" , true);
+			s.agregarPuestoDesarmable("Shaking", s.traerPersonal(35412698), 16.0f, "abcde23456", true, 1, 35);//
+			s.agregarFoodTruck("Heladito", s.traerPersonal(31258741), 26, "qwert1111", true, "AC257FT", true);//
 		}catch(Exception e) {System.out.println(e.getMessage());}
 		
-		PuestoDesarmable p = (PuestoDesarmable) s.traerUnidad("abcde12345");
-		FoodTruck f = (FoodTruck)s.traerUnidad("qwert45678");
 		
 		try {
-		s.agregarUnidadPDFestival("El Parrillon", co2, 20, "abcde12345", true, 2,75, "UnlaFest");
-		s.agregarUnidadadFTFestival("Luiggi", co2, 30, "qwert45678", true, "dsz429" , true, "UnlaFest");
+		s.agregarUnidadPDFestival("El Parrillon", s.traerPersonal(45258369), 20, "abcde12345", true, 2,75, "UnlaFest");
+		s.agregarUnidadadFTFestival("Luiggi", s.traerPersonal(46123456), 30, "qwert45678", true, "dsz429" , true, "UnlaFest");
+
+		s.agregarUnidadPDFestival("Shaking", s.traerPersonal(35412698), 16.0f, "abcde23456", true, 1, 35, "UnlaFest");//
+		s.agregarUnidadadFTFestival("Heladito", s.traerPersonal(31258741), 26, "qwert1111", true, "AC257FT", true, "UnlaFest");//
+		
 		}catch(Exception e) {System.out.println(e.getMessage());}
 		
-		System.out.println(p);
-		System.out.println(f);
-		
+		System.out.println(s.traerUnidad("abcde12345"));
+		System.out.println(s.traerUnidad("qwert45678"));		
 	//----------------------------------------
 		
-			Plato pizza = new Plato(1, "Pizza", 250.5f, 125.0f);
-			Plato carbonara = new Plato(2, "Carbonara", 500.0f, 230.0f);
-			Plato parrillada = new Plato(3, "Parrillada", 800.0f, 400.0f);
-			Plato choripan = new Plato(4, "Choripan", 250.0f, 130.0f);
-		
-		
-		
-		f.getLstPlatos().add(pizza);
-		f.getLstPlatos().add(carbonara);
-		
-		p.getLstPlatos().add(choripan);
-		p.getLstPlatos().add(parrillada);
 		
 		try {
-	s.agregarPedido(LocalDate.of(2026, 6, 11), fest.getNombre(), p.getCodigo());
-	s.agregarPedido(LocalDate.of(2026, 6, 11), fest.getNombre(), p.getCodigo());
-	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), p.getCodigo());
-	s.agregarPedido(LocalDate.of(2026, 6, 13), fest.getNombre(), p.getCodigo());
+		s.traerUnidad("qwert45678").agregarPlato( "Pizza", 20000.0f, 6850.0f);
+		s.traerUnidad("qwert45678").agregarPlato( "Carbonara", 18000.0f, 4520.0f);
+		
+		s.traerUnidad("abcde12345").agregarPlato( "Choripan", 10000.0f, 4000.0f);
+		s.traerUnidad("abcde12345").agregarPlato( "Parrillada", 45000.0f, 22000.0f);
+		
+		s.traerUnidad("abcde23456").agregarPlato("Licuado de banana", 6000.0f, 2000.0f);//
+		s.traerUnidad("abcde23456").agregarPlato("Licuado de chocolate", 6000.0f, 2000.0f);//
+		
+		s.traerUnidad("qwert1111").agregarPlato("Helado frutilla", 3200.0f, 1400.0f);//
+		s.traerUnidad("qwert1111").agregarPlato("Helado vainilla", 3200.0f, 1400.0f);//
+		
+		}catch(Exception e) {System.out.println(e.getMessage());}
+		try {
+	s.agregarPedido(LocalDate.of(2026, 6, 11), fest.getNombre(), s.traerUnidad("abcde12345").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 11), fest.getNombre(), s.traerUnidad("abcde12345").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("abcde12345").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 13), fest.getNombre(), s.traerUnidad("abcde12345").getCodigo());
 	//-
-	s.agregarPedido(LocalDate.of(2026, 6, 11), fest.getNombre(), f.getCodigo());
-	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), f.getCodigo());
-	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), f.getCodigo());
-	s.agregarPedido(LocalDate.of(2026, 6, 14), fest.getNombre(), f.getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 11), fest.getNombre(), s.traerUnidad("qwert45678").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("qwert45678").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("qwert45678").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 14), fest.getNombre(), s.traerUnidad("qwert45678").getCodigo());
+	
+	//-------------
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("abcde23456").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 13), fest.getNombre(), s.traerUnidad("abcde23456").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 14), fest.getNombre(), s.traerUnidad("abcde23456").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 14), fest.getNombre(), s.traerUnidad("abcde23456").getCodigo());
+	//-
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("qwert1111").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("qwert1111").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("qwert1111").getCodigo());
+	s.agregarPedido(LocalDate.of(2026, 6, 12), fest.getNombre(), s.traerUnidad("qwert1111").getCodigo());
+	
+	
 
 		}catch(Exception e) {System.out.println(e.getMessage());}
 		
 		
 		
-		Pedido p1 = s.traerPedido(1);
-		Pedido p2 = s.traerPedido(2);
-		Pedido p3 = s.traerPedido(3);
-		Pedido p4 = s.traerPedido(4);
-		Pedido p5 = s.traerPedido(5);
-		Pedido p6 = s.traerPedido(6);
-		Pedido p7 = s.traerPedido(7);
-		Pedido p8 = s.traerPedido(8);
 		
+		/*
 		//int idDetallePedido, Plato plato, int cantidad
-		p1.getLstDetalles().add(new DetallePedidoPlato(1, choripan, 3));
-		p2.getLstDetalles().add(new DetallePedidoPlato(2, parrillada, 1));
-		p3.getLstDetalles().add(new DetallePedidoPlato(3, choripan, 1));
-		p4.getLstDetalles().add(new DetallePedidoPlato(4, parrillada, 2));
+		s.traerPedido(1).getLstDetalles().add(new DetallePedidoPlato(1, choripan, 3));
+		s.traerPedido(2).getLstDetalles().add(new DetallePedidoPlato(2, parrillada, 1));
+		s.traerPedido(3).getLstDetalles().add(new DetallePedidoPlato(3, choripan, 1));
+		s.traerPedido(4).getLstDetalles().add(new DetallePedidoPlato(4, parrillada, 2));
 		//--
-		p5.getLstDetalles().add(new DetallePedidoPlato(5, carbonara, 2));
-		p6.getLstDetalles().add(new DetallePedidoPlato(6, pizza, 1));
-		p7.getLstDetalles().add(new DetallePedidoPlato(7, pizza, 4));
-		p8.getLstDetalles().add(new DetallePedidoPlato(8, carbonara,2 ));
+		s.traerPedido(5).getLstDetalles().add(new DetallePedidoPlato(5, carbonara, 2));
+		s.traerPedido(6).getLstDetalles().add(new DetallePedidoPlato(6, pizza, 1));
+		s.traerPedido(7).getLstDetalles().add(new DetallePedidoPlato(7, pizza, 4));
+		s.traerPedido(8).getLstDetalles().add(new DetallePedidoPlato(8, carbonara,2 ));
 		
+		*/
+		try {
+		s.traerPedido(1).agregarDetallePedido(s.traerUnidad("abcde12345").traerPlato("Choripan"), 3);
+		s.traerPedido(2).agregarDetallePedido(s.traerUnidad("abcde12345").traerPlato("Parrillada"), 1);
+		s.traerPedido(3).agregarDetallePedido(s.traerUnidad("abcde12345").traerPlato("Choripan"), 1);
+		s.traerPedido(4).agregarDetallePedido(s.traerUnidad("abcde12345").traerPlato("Parrillada"), 2);
 		
+		s.traerPedido(5).agregarDetallePedido(s.traerUnidad("qwert45678").traerPlato("carbonara"), 2);
+		s.traerPedido(6).agregarDetallePedido(s.traerUnidad("qwert45678").traerPlato("pizza"), 1);
+		s.traerPedido(7).agregarDetallePedido(s.traerUnidad("qwert45678").traerPlato("pizza"), 4);
+		s.traerPedido(8).agregarDetallePedido(s.traerUnidad("qwert45678").traerPlato("carbonara"), 2);
 		
+		s.traerPedido(9).agregarDetallePedido(s.traerUnidad("abcde23456").traerPlato("Licuado de banana"),2);
+		s.traerPedido(10).agregarDetallePedido(s.traerUnidad("abcde23456").traerPlato("Licuado de chocolate"),1);
+		s.traerPedido(11).agregarDetallePedido(s.traerUnidad("abcde23456").traerPlato("Licuado de banana"),3);
+		s.traerPedido(12).agregarDetallePedido(s.traerUnidad("abcde23456").traerPlato("Licuado de chocolate"),1);
 		
+		s.traerPedido(13).agregarDetallePedido(s.traerUnidad("qwert1111").traerPlato("Helado vainilla"),2);
+		s.traerPedido(14).agregarDetallePedido(s.traerUnidad("qwert1111").traerPlato("Helado frutilla"),1);
+		s.traerPedido(15).agregarDetallePedido(s.traerUnidad("qwert1111").traerPlato("Helado vainilla"),3);
+		s.traerPedido(16).agregarDetallePedido(s.traerUnidad("qwert1111").traerPlato("Helado frutilla"),1);
+		}catch(Exception e) {System.out.println(e.getMessage());}
 
 //----------------------- casos de uso
 		
 		//calculo de canon: 
-		float canonF = f.calcularCanon();
+		float canonF = s.traerUnidad("qwert45678").calcularCanon();
 		
 		System.out.println("El canon del food truck es: " + canonF);
 		
-		Float canonP = p.calcularCanon();
+		float canonP = s.traerUnidad("abcde12345").calcularCanon();
 		
 		System.out.println("El canon del puesto desarmable es: " + canonP);
+		
+		float  canonF2 = s.traerUnidad("abcde23456").calcularCanon();
+		
+		System.out.println("El canon del puesto desarmable es: " + canonF2);
+		
+		float canonP2 = s.traerUnidad("qwert1111").calcularCanon();
+		System.out.println("El canon del puesto desarmable es: " + canonP2);
 
 	//---------------- liquidacion de haberes
 		
-		float hab1, hab2, hab3, hab4;
-		hab1 = c1.liquidarHaberes();
-		hab2 = c2.liquidarHaberes();
-		hab3 = co1.liquidarHaberes();
-		hab4 = co2.liquidarHaberes();
 		
-		System.out.println("Haberes liquidados cajero 1: "+hab1);
-		System.out.println("Haberes liquidados cajero 2: "+hab2);
-		System.out.println("Haberes liquidados cocinero 1: "+hab3);
-		System.out.println("Haberes liquidados cocinero 2: "+hab4);
+		
+		System.out.println("Haberes liquidados cajero 1: "+s.traerPersonal(47130790).liquidarHaberes());
+		System.out.println("Haberes liquidados cajero 2: "+s.traerPersonal(47130890).liquidarHaberes());
+		System.out.println("Haberes liquidados cajero 3: "+s.traerPersonal(45263123).liquidarHaberes());
+		System.out.println("Haberes liquidados cajero 4: "+s.traerPersonal(36211589).liquidarHaberes());
+		System.out.println("Haberes liquidados cocinero 1: "+s.traerPersonal(45258369).liquidarHaberes());
+		System.out.println("Haberes liquidados cocinero 2: "+s.traerPersonal(46123456).liquidarHaberes());
+		System.out.println("Haberes liquidados cocinero 3: "+s.traerPersonal(35412698).liquidarHaberes());
+		System.out.println("Haberes liquidados cocinero 4: "+s.traerPersonal(31258741).liquidarHaberes());
 		
 	//---------------------- Reporte de recaudacion
 		
@@ -151,6 +192,33 @@ public class Test {
 		System.out.println(s.reporteRecaudacion("UnlaFest"));
 		
 		//3400...3250
-}
+		
+	//---------------------- Filtro de personal por edad
+		System.out.println(s.traerPersonalEdad(LocalDate.of(2000, 1, 1), LocalDate.of(2001, 12, 31)));
+		
+		
+	//---------------------- Calculo de rentabilidad neta	
+	System.out.println(s.calcularGanancia(1));
+	System.out.println(s.calcularGanancia(2));
+	System.out.println(s.calcularGanancia(3));//
+	System.out.println(s.calcularGanancia(4));//
+	
+	//---------------------- Ranking de unidades
+	System.out.println("Ranking de unidades: " + s.ordenarUnidadesRecaudaciones());
+	
+	//---------------------- Plato estrella 
+	System.out.println("Plato estrella de 'El Parrillon': " +s.traerPlatoEstrella(1, "UnlaFest"));
+	System.out.println("Plato estrella de Luiggi: " + s.traerPlatoEstrella(2, "UnlaFest"));
+	System.out.println("Plato estrella de Shaking: " + s.traerPlatoEstrella(3, "UnlaFest"));//
+	System.out.println("Plato estrella de Heladito: " + s.traerPlatoEstrella(4, "UnlaFest"));//
+	
+		
+	//---------------------- Auditoria del personal: 
+	List<Personal> listaP = s.traerPersonalFestival("UnlaFest");
+	System.out.println(listaP);
+		
+	}
+	
+
 
 }
